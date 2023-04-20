b2b702129baf2f0de8e218f63d585e2ed3f11ea8
Trust-Ekpo <tekpo24@gmail.com>
2023-04-20 14:45:15 +0100

    first commit

---
+++ check_user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#get the path to the list of users
+echo "What is the path to the list of users you want to check?"
+read path
+
+#check to see is the variable path is not empty
+if [ -n $path ]
+then
+	#loop through the list of users and perfom id on each
+	for i in `cat $path` ; do id $i ; done
+
+fi
---
+++ create_users.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "What is the path to the list of users you want to create?"
+read path
+
+#check to ensure the variable path is not empty
+if [ -n $path ]
+then
+	#loop thorugh the list of names and  create a user. 
+	#-m is used to create a home directory of each user
+	for i in `cat $path` ; do useradd -m $i ; done
+
+fi
